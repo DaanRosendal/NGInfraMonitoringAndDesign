2a35dc91c692ba6981ef5e189e824cd7bbb7d1b4
MrWouterNL <mail@mrwouter.nl>
2021-04-30 13:24:51 +0200

    Automatically update server status

--- src/com/nerdygadgets/monitoring/MonitoringFrame.java
+++ src/com/nerdygadgets/monitoring/MonitoringFrame.java
@@ -4,38 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MonitoringFrame extends JFrame implements ActionListener {
 
+    private MonitoringPanel panel;
+
     public MonitoringFrame(Server... servers) {
         setTitle("NerdyGadgets Infra Monitoring");
         setSize(600, 300);
-        setLayout(new GridLayout(servers.length, 3));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
 
-        for (Server server : servers) {
-
-            JLabel serverInfo = new JLabel(server.getName() + " (" + server.getIp() + ")");
-            if (server.isOnline()) {
-                serverInfo.setForeground(Color.GREEN);
-                this.add(serverInfo);
-                this.add(new JLabel("CPU: " + server.getCpuUsage() + "%"));
-                this.add(new JLabel("Storage: " + server.getDiskMbUsed() + "mb/" + server.getDiskMbTotal() + "mb"));
-            } else {
-                serverInfo.setForeground(Color.RED);
-                this.add(serverInfo);
-                this.add(new JLabel("OFFLINE!"));
-                this.add(new JLabel("OFFLINE!"));
-            }
-        }
+        this.panel = new MonitoringPanel(servers);
+        this.add(this.panel);
 
         setVisible(true);
 
         setLocationRelativeTo(null);
 
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Arrays.stream(servers).forEach(Server::retrieveData);
+                panel.update();
+            }
+        }, 3 * 1000L, 3 * 1000L);
     }
 
     @Override
